99d7ae82bc0da951f5a2f7b9d4ffff9b9962169e
ankit <am245895@gmail.com>
2020-09-06 02:31:49 +0530

    date time with django

--- course/views.py
+++ course/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from datetime import datetime
 def l_py(request):
     coursename="python"
     duration="7 days"
     seet="10 seets"
-    py_data ={'cname':coursename,'du':duration,'st':seet}
+    dt=datetime.now()
+    py_data ={'cname':coursename,'du':duration,'st':seet,'d':dt}
 
     return render(request,'course/c1.html',py_data)
 def l_dj(request):
